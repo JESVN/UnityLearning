dcc7d7044ad0237f62c1aa75da9e6009b0046478
JESVN <1501766557@qq.com>
2023-05-25 23:20:09 +0800

    1.平滑移动： 1.移动方式切换修改，新增一个枚举进行切换 2.修改README.md

--- UnityLearning/Assets/Scripts/01LerpMove/LerpMove.cs
+++ UnityLearning/Assets/Scripts/01LerpMove/LerpMove.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class LerpMove : MonoBehaviour
 {
+    public enum TChange
+    {
+        None,
+        Change0,
+        Change1,
+        Change2,
+    }
+    [SerializeField]TChange change;
     [SerializeField] float moveTime = 1f;
     float animTime;
     bool isAnimating;
@@ -16,16 +24,37 @@ public class LerpMove : MonoBehaviour
             CheckInput();
     }
 
+
     void Animate()
     {
         animTime += Time.deltaTime;
         float t = Mathf.Clamp01(animTime / moveTime);
 
-        t = CubicInOut(t);
+        ChangeT(ref t);
 
         transform.position = Vector3.Lerp(posStart, posTarget, t);
         if (t >= 1f)
             isAnimating = false;
+    }
+
+    void ChangeT(ref float t)
+    {
+        switch (change)
+        {
+            case TChange.None:
+                break;
+            case TChange.Change0:
+                t = QuaEaseIn(t);
+                break;
+            case TChange.Change1:
+                t = QuaEaseOut(t);
+                break;
+            case TChange.Change2:
+                t = CubicInOut(t);
+                break;
+            default:
+                break;
+        }
     }
 
     float QuaEaseIn(float t) => t * t;
